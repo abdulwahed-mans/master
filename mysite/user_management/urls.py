313c93229d8b919d40863f9b7bcaff2d992f7037
Abdulwahed Mansour <153433760+abdulwahed-mans@users.noreply.github.com>
2024-01-29 01:28:01 +0100

    Change profile for user

--- mysite/user_management/urls.py
+++ mysite/user_management/urls.py
@@ -1,31 +1,43 @@
-# user_management/urls.py 
+# user_management/urls.py
 from django.contrib import admin
 from django.urls import path, include
-# Add the following 
 from django.contrib.auth import views as auth_views
-from users.views import CustomLoginView  
+from users.views import CustomLoginView, ResetPasswordView, ChangePasswordView
 from users.forms import LoginForm
-
-from django.urls import path
-from users.views import ResetPasswordView
-
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('users.urls')),  # This is what we added
-    # Add this path
-    path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
-                                           authentication_form=LoginForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
-    path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-         name='password_reset_confirm'),
-    path('password-reset-complete/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
-         name='password_reset_complete'),
+    path("admin/", admin.site.urls),
+    path("", include("users.urls")),
+    path(
+        "login/",
+        CustomLoginView.as_view(
+            redirect_authenticated_user=True,
+            template_name="users/login.html",
+            authentication_form=LoginForm,
+        ),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        name="logout",
+    ),
+    path("password-reset/", ResetPasswordView.as_view(), name="password_reset"),
+    path(
+        "password-reset-confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+    path("password-change/", ChangePasswordView.as_view(), name="password_change"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -11,6 +11,11 @@ from django.contrib.messages.views import SuccessMessageMixin
 
 from django.contrib.auth.decorators import login_required
 
+from .forms import UpdateUserForm, UpdateProfileForm
+
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.messages.views import SuccessMessageMixin
+
 
 
 def home(request):
@@ -82,6 +87,32 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
 
 
 
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'users/change_password.html'
+    success_message = "Successfully Changed Your Password"
+    success_url = reverse_lazy('users-home')
+
+
+
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == "POST":
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your profile is updated successfully")
+            return redirect(to="users-profile")
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        profile_form = UpdateProfileForm(instance=request.user.profile)
+
+    return render(
+        request,
+        "users/profile.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
